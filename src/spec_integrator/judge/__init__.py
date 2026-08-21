86ed642feb5c8fdf37e3a0417d54b955d8e1c55f
kmt-t <t-matsu@remoteplace.net>
2026-08-21 12:47:25 +0900

    refactor(judge): rename llm.py to semantic_judge.py and organize judge package exports

---
+++ src/spec_integrator/judge/__init__.py
@@ -0,0 +1,11 @@
+from spec_integrator.judge.semantic_judge import SemanticJudge, JudgeResult, LLMJudge
+from spec_integrator.judge.risk_assessor import RiskAssessor, SectionRiskAssessment, RiskAssessmentReport
+
+__all__ = [
+    "SemanticJudge",
+    "JudgeResult",
+    "LLMJudge",
+    "RiskAssessor",
+    "SectionRiskAssessment",
+    "RiskAssessmentReport",
+]
